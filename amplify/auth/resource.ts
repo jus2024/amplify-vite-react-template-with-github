2ec00470fd0a2447d6ca3a6dede28f9d124d3d86
ジャス <az.systems.2024@gmail.com>
2026-05-23 22:33:04 +0900

    setup: resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,34 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      oidc: [
+        {
+          name: "GitHub",
+          clientId: secret("GITHUB_CLIENT_ID"),
+          clientSecret: secret("GITHUB_CLIENT_SECRET"),
+          // ★ 手順 4 で取得した API Gateway URL を設定
+          issuerUrl: "https://vyzf29a9ki.execute-api.us-west-2.amazonaws.com/prod",
+          scopes: ["openid", "read:user", "user:email"],
+          attributeMapping: {
+            email: "email",
+            preferredUsername: "preferred_username",
+            profilePicture: "picture",
+          },
+        },
+      ],
+      callbackUrls: [
+        "http://localhost:3000/",
+        // ★ 手順 4 で生成された Amplify ドメインの URL を設定
+        "https://main.dmyossuuu34g0.amplifyapp.com",
+      ],
+      logoutUrls: [
+        "http://localhost:3000/",
+        // ★ 手順 4 で生成された Amplify ドメインの URL を設定
+        "https://main.dmyossuuu34g0.amplifyapp.com",
+      ],
+    },
   },
 });
